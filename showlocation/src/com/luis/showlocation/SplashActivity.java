3c1f9b5edf65dcee41c42dfba109ddce616feab6
lv910929 <lv910929@live.com>
2016-03-16 09:48:13 +0800

    第一次上传

---
+++ showlocation/src/com/luis/showlocation/SplashActivity.java
@@ -0,0 +1,71 @@
+package com.luis.showlocation;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+	
+	public static final String TAG = SplashActivity.class.getSimpleName();
+
+	private ImageView mSplashItem_iv = null;
+	
+	protected Handler mHandler = null;
+
+	protected void findViewById() {
+		// TODO Auto-generated method stub
+		mSplashItem_iv = (ImageView) findViewById(R.id.splash_loading_item);
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		mHandler = new Handler(getMainLooper());
+		findViewById();
+		initView();
+	}
+
+	protected void initView() {
+		// TODO Auto-generated method stub
+		Animation translate = AnimationUtils.loadAnimation(this,
+				R.anim.splash_loading);
+		translate.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+				startActivity(intent);
+				overridePendingTransition(R.anim.push_left_in,
+						R.anim.push_left_out);
+				SplashActivity.this.finish();
+			}
+		});
+		mSplashItem_iv.setAnimation(translate);
+	}
+	
+
+
+}
